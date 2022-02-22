7751076d8acbd2cd2bb091e21cd609fc46a4e483
vijay <vijaygohel5454@gmail.com>
2022-02-22 12:48:33 +0530

    used mongodb and heroku deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const _ = require('lodash');
+const mongoose= require("mongoose");
 
 const app = express();
 
@@ -9,20 +9,33 @@ app.use(bodyparser.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
 
-const posts=[];
+// const posts=[];
+mongoose.connect("mongodb://localhost:27017/TodoList");
 
-app.listen(3000, ()=>{
+const postSchema = {
+    title:String ,
+    content:String
+}
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server started on port 3000");
 })
 
 app.get("/",(req,res)=>{
-    res.render("home",{todoItems:posts}
-    );
+    Post.find((err,posts)=>{
+        if(!err)
+            res.render("home",{todoItems:posts});
+    })
+    
 })
 
 app.get("/compose",(req,res)=>
 {
-    res.render("compose")
+    res.render("compose");
 })
 
 app.get("/contact",(req,res)=>
@@ -35,16 +48,15 @@ app.get("/about",(req,res)=>
     res.render("about")
 })
 
-app.get('/posts/:postTitle', (req, res) => {
-    function findPost(post) {
-        return _.kebabCase(post.title) === _.kebabCase(req.params.postTitle);
-      }
-      const found = posts.find(findPost);
+app.get('/posts/:postId', (req, res) => {
+       Post.find({_id : req.params.postId},(err,found)=>{     
+        if(found != undefined)
+            res.render("post",{Post:found[0]});
+        else
+            res.render("error404");
+       });
 
-      if(found != undefined)
-        res.render("post",{Post:found});
-      else
-        res.render("error404");
+      
   })
 
  app.get('*', function(req, res){
@@ -55,12 +67,13 @@ app.post("/", (req,res)=>{
     
     let postTitle =req.body.title;
     let postContent = req.body.content;
-    postTitle = postTitle;
 
-    if(postContent !='' || postTitle !='')
-        posts.push({title: postTitle, content:postContent});
-
-    console.log(posts);
-
-    res.redirect("/");
+    if(postContent !='' || postTitle !=''){
+        // posts.push({title: postTitle, content:postContent});
+        const post = new Post({title:postTitle , content:postContent });
+        post.save((err)=>{
+            if(!err)
+                res.redirect("/");
+        });
+    }
 })
